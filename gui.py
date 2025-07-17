ecb4d7b326df7e8538dbcc30728486fe08da0982
Selin Yeniay <yeniay@gmx.de>
2025-07-17 14:05:18 +0200

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(140, 180, 135, 76))
+        self.verticalLayout = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.calculateButton = QPushButton(self.layoutWidget)
+        self.calculateButton.setObjectName(u"calculateButton")
+
+        self.verticalLayout.addWidget(self.calculateButton)
+
+        self.outputField = QLabel(self.layoutWidget)
+        self.outputField.setObjectName(u"outputField")
+
+        self.verticalLayout.addWidget(self.outputField)
+
+        self.inputField = QLineEdit(self.layoutWidget)
+        self.inputField.setObjectName(u"inputField")
+
+        self.verticalLayout.addWidget(self.inputField)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.calculateButton.setText(QCoreApplication.translate("MainWindow", u"Berechnen", None))
+        self.outputField.setText(QCoreApplication.translate("MainWindow", u"Ausgabe hier", None))
+        self.inputField.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Eingabe hier eingeben", None))
+    # retranslateUi
+
