075e8bcfebd0c705ded989dab550cecb1b5c28f4
Selin Yeniay <yeniay@gmx.de>
2025-07-17 17:33:03 +0200

    Update gui.py
    from ui_weather_app,gui

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 
 ################################################################################
 ## Form generated from reading UI file 'weather_app.ui'
